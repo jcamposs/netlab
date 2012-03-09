fcd40ced0902feda06ae349c101452fa06d571ed
Santiago Carot-Nemesio <sancane@gmail.com>
2012-03-09 14:32:45 +0100

    Create rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netlab::Application.config.secret_token = 'f9927eb939e50dad99e5bf52d7a41018d6713e8a3f7e4e56ef474377ff38b3c7828fc147c06d6181d6df75d432f6e1c8d096be30d5a0cd1069d8268870c12310'
