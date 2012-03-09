caab8519c2caa3ce8a80823a37333389a61e5036
Santiago Carot-Nemesio <sancane@gmail.com>
2012-03-09 19:16:35 +0100

    Add missing test to fix devise error.
    This patch add fixtures data wich is used in Devise tests. It also
    fix a problem with authenticate unit test so it avoid crashes during
    the execution of tests.
    
    This patch requires feeding the development data base with fixture data:
    rake db:seed

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   test "should get index" do
+    sign_in users(:homer)
     get :index
     assert_response :success
   end
