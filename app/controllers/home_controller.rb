8daea8ec40986836afdb61b0ed453af091c5dca4
Santiago Carot-Nemesio <sancane@gmail.com>
2012-03-13 16:17:28 +0100

    Start shell in a box when user logins

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,12 @@ class HomeController < ApplicationController
 
   def index
     @user = current_user
+
+    #TODO: Calculate the url for shell in a box
+    @shell_url = "http://localhost:4200"
+    @pid = pid = fork do
+      #TODO: Configure the next comand
+      exec "shellinaboxd -t"
+    end
   end
 end
