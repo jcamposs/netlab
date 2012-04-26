099c4888d54e57cb2ffd420170eaaaa6c0375919
Santiago Carot-Nemesio <sancane@gmail.com>
2012-04-26 12:07:58 +0200

    Simple push commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @user = current_user
 
     #TODO: Calculate the url for shell in a box
+
     @shell_url = "http://localhost:4200"
     @pid = pid = fork do
       #TODO: Configure the next comand
