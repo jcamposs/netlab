c6123c471917006681d0ce412fbf7341a8354820
Santiago Carot Nemesio <sancane@gmail.com>
2012-08-08 11:59:55 +0200

    Use kineticjs v.3.10.5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require kinetic-v3.10.3
+//= require kinetic-v3.10.5
 //= require kwidgets
 //= require netlab
 //= require desktopeditor
