09e49c270f0101158241bfa623443b8159fe09e1
Santiago Carot Nemesio <sancane@gmail.com>
2012-08-08 16:15:08 +0200

    Remove version from kinetic library name

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require kinetic-v3.10.5
+//= require kinetic
 //= require kwidgets
 //= require netlab
 //= require desktopeditor
