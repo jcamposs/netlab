bb5f68481c5df34170f5736183ab83a2c116b6f6
Santiago Carot Nemesio <sancane@gmail.com>
2012-09-25 13:00:56 +0200

    Add rough implementation for change_state method

--- app/controllers/vm_notification_controller.rb
+++ app/controllers/vm_notification_controller.rb
@@ -1,5 +1,19 @@
 class VmNotificationController < ApplicationController
+
+  #PUT /vm_notification/1/change_state
   def change_state
-    #TODO: Set new virtual_machine state
+    vm = VirtualMachine.find(params[:id])
+    #TODO: Get proper state sent from proxy
+    state = "running"
+    vm.state = state
+
+    respond_to do |format|
+      if vm.save
+        format.html { render :nothing => true, status: :ok}
+      else
+        format.html { render :nothing => true, status: :unprocessable_entity }
+      end
+    end
   end
+
 end
