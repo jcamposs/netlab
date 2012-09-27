bd07d1746f7d7aa0b211503f1662ee9fc658a5c4
Santiago Carot Nemesio <sancane@gmail.com>
2012-09-27 12:59:47 +0200

    Set virtual machine state notificated by the proxy

--- app/controllers/vm_notification_controller.rb
+++ app/controllers/vm_notification_controller.rb
@@ -2,14 +2,12 @@ class VmNotificationController < ApplicationController
 
   #PUT /vm_notification/1/change_state
   def change_state
-    vm = VirtualMachine.find(params[:id])
-    #TODO: Get proper state sent from proxy
-    state = "running"
-    vm.state = state
+    vm = VirtualMachine.find_by_name_and_workspace_id(params["virual_machine"], params["workspace_id"])
+    vm.state = params["state"]
 
     respond_to do |format|
       if vm.save
-        format.html { render :nothing => true, status: :ok}
+        format.html { render :nothing => true, status: :ok }
       else
         format.html { render :nothing => true, status: :unprocessable_entity }
       end
