085895dbcfeb269fe2012833964f6041365c55bb
Santiago Carot Nemesio <sancane@gmail.com>
2012-09-27 12:58:15 +0200

    Change initial virtual machine state to "starting"

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -286,7 +286,7 @@ class WorkspacesController < ApplicationController
   def configure_virtual_machine(vm, port)
     return false if not vm
 
-    vm.state = "booting"
+    vm.state = "starting"
     vm.port_number = port
     vm.save
   end
