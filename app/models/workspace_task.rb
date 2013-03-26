63f532e69a42526096c68b0dc0fd7bfc38c8e0ea
Raul Roman <rroman@libresoft.es>
2013-03-26 18:02:15 +0100

    Ranges modified and introduction of helpers

--- app/models/workspace_task.rb
+++ app/models/workspace_task.rb
@@ -6,6 +6,36 @@ class WorkspaceTask < ActiveRecord::Base
 
   belongs_to :workspace
 
-  validates :priority, :inclusion => 0..5
-  validates :state, :inclusion => 0..5
+  validates :priority, :inclusion => 0..4
+  validates :state, :inclusion => 0..4
+
+  def get_priority
+    case self.priority
+    when 0
+      return "Very low"
+    when 1
+      return "Low"
+    when 2
+      return "Medium"
+    when 3
+      return "High"
+    when 4
+      return "Very high"
+    end
+  end
+
+  def get_status
+    case self.state
+    when 0
+      return "Open"
+    when 1
+      return "Resolved"
+    when 2
+      return "Feedback"
+    when 3
+      return "Fixed"
+    when 4
+      return "Closed"
+    end
+  end
 end
